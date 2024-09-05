c2a8e7f8ca1f8efe8e304a4974c8c75354a347d8
Marl0023 <marlotay2312@gmail.com>
2024-09-05 09:32:26 -0500

    Primer salvado

---
+++ clase4/Program.cs
@@ -0,0 +1,10 @@
+﻿int N, a = 0, b = 1, c;
+Console.Write("Ingrese el limite de resultados que desea: ");
+N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++) {
+    Console.WriteLine($"F{i + 1}: {a}");
+    c = a + b;
+    a = b;
+    b = c;
+}
+Console.ReadKey();
